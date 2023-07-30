e2dd1f4b1e10e8733055b265e3e5977e3d18d6a7
Max <maximdalat@gmail.com>
2023-07-30 08:14:13 -0700

    Initial Commit

---
+++ Productivity/ProductivityApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProductivityApp.swift
+//  Productivity
+//
+//  Created by Maxim Dalat on 7/30/23.
+//
+
+import SwiftUI
+
+@main
+struct ProductivityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
